05858ab4af4acfdd3236e98931eaf8a2279df467
j1mmyson <bwson98@gmail.com>
2021-10-27 13:17:14 +0900

    User 테이블 구현

---
+++ model/model.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID       uuid.UUID `gorm:"type:uuid"`
+	Password string
+	Name     string
+}
