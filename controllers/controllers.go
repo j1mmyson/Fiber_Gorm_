9f1a03686d017cb856f7a2ebe8a86505c12723f1
j1mmyson <bwson98@gmail.com>
2021-11-09 14:34:34 +0900

    CRUD 구현

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -24,3 +24,167 @@ func GetUsers(c *fiber.Ctx) error {
 
 	return c.JSON(fiber.Map{"status": "success", "data": users})
 }
+
+// function to create a new user
+func CreateUser(c *fiber.Ctx) error {
+	db := database.DB
+	user := new(model.User)
+
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": user})
+}
+
+// function to get a user by id
+func GetUser(c *fiber.Ctx) error {
+	db := database.DB
+	user := new(model.User)
+
+	if err := db.First(&user, c.Params("id")).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": user})
+}
+
+// function to update a user by id
+func UpdateUser(c *fiber.Ctx) error {
+	db := database.DB
+	user := new(model.User)
+
+	if err := db.First(&user, c.Params("id")).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := db.Save(&user).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": user})
+}
+
+// function to delete a user by id
+func DeleteUser(c *fiber.Ctx) error {
+	db := database.DB
+	user := new(model.User)
+
+	if err := db.First(&user, c.Params("id")).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := db.Delete(&user).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": user})
+}
+
+// function to create a new article
+func CreateArticle(c *fiber.Ctx) error {
+	db := database.DB
+	article := new(model.Article)
+
+	if err := c.BodyParser(article); err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := db.Create(&article).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": article})
+}
+
+// function to get all articles
+func GetArticles(c *fiber.Ctx) error {
+	db := database.DB
+	var articles []model.Article
+
+	if err := db.Find(&articles).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": articles})
+}
+
+// function to get an article by id
+func GetArticle(c *fiber.Ctx) error {
+	db := database.DB
+	article := new(model.Article)
+
+	if err := db.First(&article, c.Params("id")).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": article})
+}
+
+// function to update an article by id
+func UpdateArticle(c *fiber.Ctx) error {
+	db := database.DB
+	article := new(model.Article)
+
+	if err := db.First(&article, c.Params("id")).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := c.BodyParser(article); err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := db.Save(&article).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": article})
+}
+
+// function to delete an article by id
+func DeleteArticle(c *fiber.Ctx) error {
+	db := database.DB
+	article := new(model.Article)
+
+	if err := db.First(&article, c.Params("id")).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	if err := db.Delete(&article).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": article})
+}
+
+// function to find articles by tag
+func GetArticlesByTag(c *fiber.Ctx) error {
+	db := database.DB
+	var articles []model.Article
+
+	if err := db.Where("tag = ?", c.Params("tag")).Find(&articles).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": articles})
+}
+
+// function to find articles by author
+func GetArticlesByAuthor(c *fiber.Ctx) error {
+	db := database.DB
+	var articles []model.Article
+
+	if err := db.Where("author = ?", c.Params("author")).Find(&articles).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "data": articles})
+}
